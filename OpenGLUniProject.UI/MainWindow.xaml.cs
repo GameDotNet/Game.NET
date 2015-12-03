dd3b701c1c0450e2e4bc6c3a81ad40380b875488
Łukasz Pyrzyk <l.pyrzyk@outlook.com>
2015-12-03 18:46:39 +0100

    OpenGL control

--- OpenGLUniProject.UI/MainWindow.xaml.cs
+++ OpenGLUniProject.UI/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using SharpGL.SceneGraph;
 
 namespace OpenGLUniProject.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// OpenGL handler
+        /// </summary>
+        /// <param name="sender">OpenGL control</param>
+        /// <param name="args">Entry arguments</param>
+        private void OpenGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
+        {
         }
     }
 }
