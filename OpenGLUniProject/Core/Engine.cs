1844f0709289b0003cedb6422878f01482c00f1a
Łukasz Pyrzyk <l.pyrzyk@outlook.com>
2015-12-03 21:51:02 +0100

    Renderer with _

--- OpenGLUniProject/Core/Engine.cs
+++ OpenGLUniProject/Core/Engine.cs
@@ -110,7 +110,7 @@ namespace OpenGLUniProject.Core
         {
             _renderer.Begin();
             {
-                renderer.TempDraw();
+                _renderer.TempDraw();
             }
             _renderer.End();
         }
