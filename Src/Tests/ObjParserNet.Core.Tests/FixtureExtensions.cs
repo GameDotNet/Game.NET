0697e9ca787b0107ba6c1bd3f1bfc39b9ee43faf
MrJaqbq <bentkowski.jakub@gmail.com>
2016-01-18 20:50:33 +0100

    Adjusted unit test setups to OpenTK

---
+++ Src/Tests/ObjParserNet.Core.Tests/FixtureExtensions.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK;
+using Ploeh.AutoFixture;
+
+namespace ObjParserNet.Core.Tests
+{
+    public static class FixtureExtensions
+    {
+        public static void OmitVector3Properties(this Fixture fixture)
+        {
+            fixture.Customize<Vector3>(composer => composer
+                .Without(v => v.Xy)
+                .Without(v => v.Xz)
+                .Without(v => v.Yx)
+                .Without(v => v.Yz)
+                .Without(v => v.Zx)
+                .Without(v => v.Zy)
+                .Without(v => v.Xzy)
+                .Without(v => v.Yxz)
+                .Without(v => v.Yzx)
+                .Without(v => v.Zxy)
+                .Without(v => v.Zyx));
+        }
+    }
+}
