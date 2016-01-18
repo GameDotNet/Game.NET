a92e165dcdf201c2ddbd3e11fcaca1081f411872
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-18 19:32:04 +0100

    FaceItem unit tests

---
+++ Src/Tests/ObjParserNet.Core.Tests/Model/FaceItemTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace ObjParserNet.Core.Tests.Model
+{
+    [TestFixture]
+    public class FaceItemTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        [Test]
+        public void CanInitializePropertiesByConstructor()
+        {
+            FaceItem faceItem = new FaceItem();
+
+            Assert.That(faceItem.Normal, Is.EqualTo(default(uint)));
+            Assert.That(faceItem.Texture, Is.EqualTo(default(uint)));
+            Assert.That(faceItem.Vertex, Is.EqualTo(default(uint)));
+        }
+
+        [Test]
+        public void CanInitializeAllPropertiesByConstructor()
+        {
+            uint vertex = _fixture.Create<uint>();
+            uint texture = _fixture.Create<uint>();
+            uint normal = _fixture.Create<uint>();
+
+            FaceItem faceItem = new FaceItem(vertex, texture, normal);
+
+            Assert.That(faceItem.Normal, Is.EqualTo(normal));
+            Assert.That(faceItem.Texture, Is.EqualTo(texture));
+            Assert.That(faceItem.Vertex, Is.EqualTo(vertex));
+        }
+    }
+}
