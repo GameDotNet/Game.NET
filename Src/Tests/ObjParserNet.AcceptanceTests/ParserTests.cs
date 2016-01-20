0b9657c2ae7efae266fad8fb97a0488078c4d1f2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-20 18:58:27 +0100

    Acceptance test for Parser

---
+++ Src/Tests/ObjParserNet.AcceptanceTests/ParserTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using ObjParserNet.Core;
+
+namespace ObjParserNet.AcceptanceTests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        [Test]
+        public void CanLoadSampleObject()
+        {
+            string fileName = "box.obj";
+            string path = $"{AssemblyDirectory}\\FakeData\\{fileName}";
+
+            Parser.Parser p = new Parser.Parser();
+            Mesh mesh = p.LoadMesh(path);
+
+            Assert.That(mesh, Is.Not.Null);
+            Assert.That(mesh.Filename, Is.EqualTo(fileName));
+
+            // object
+            Assert.That(mesh.SubMeshes.Count, Is.EqualTo(1));
+
+            // vertex
+            SubMesh submesh = mesh.SubMeshes.First();
+            Assert.That(submesh.Vertices.Count, Is.EqualTo(12));
+
+            // normal
+            Assert.That(submesh.Normals.Count, Is.EqualTo(6));
+
+            // face
+            Assert.That(submesh.Faces.Count, Is.EqualTo(20));
+        }
+    }
+}
