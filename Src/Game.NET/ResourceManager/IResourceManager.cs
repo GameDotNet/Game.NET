472d3a92e013240c3759ea4d012fc8abc809626c
michalwisniowski <micwisniowski@gmail.com>
2016-05-19 10:46:36 +0200

    changed static methods to normal, created interface for ResourceManager for future DI usage

---
+++ Src/Game.NET/ResourceManager/IResourceManager.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.NET
+{
+    public interface IResourceManager
+    {
+        T Get<T>(string name) where T : Resource;
+
+        T GetOrCreate<T>(string name) where T : Resource, new();
+
+        T Insert<T>(string name, T resource) where T : Resource;
+
+        bool Contains<T>(string name) where T : Resource;
+
+        void Remove<T>(T resource) where T : Resource;
+
+        void Remove<T>(string name) where T : Resource;
+
+        void CleanAll();
+    }
+}
