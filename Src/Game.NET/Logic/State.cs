3269a1f3d558d69032fe5904583718213cb37cfb
MrJaqbq <bentkowski.jakub@gmail.com>
2016-05-23 18:08:38 +0200

    Added Menu classes

---
+++ Src/Game.NET/Logic/State.cs
@@ -0,0 +1,24 @@
+﻿using Game.NET.Core;
+
+namespace Game.NET.Logic
+{
+    public abstract class State
+    {
+        protected Engine _engine;
+
+        public State(Engine engine)
+        {
+            _engine = engine;
+        }
+        
+        public abstract void Start();
+
+        public abstract void End();
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void ProcessInput();
+
+        public abstract void CleanUp();
+    }
+}
