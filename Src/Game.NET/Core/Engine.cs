a0f83e2a9c1e6fa587c14bc3a6a739854fec4ed0
MrJaqbq <bentkowski.jakub@gmail.com>
2016-05-23 18:08:38 +0200

    Added Menu classes

---
+++ Src/Game.NET/Core/Engine.cs
@@ -0,0 +1,38 @@
+﻿using Game.NET.Logic;
+
+namespace Game.NET.Core
+{
+    public class Engine
+    {
+        public GameTime Time { get; private set; }
+
+        public State CurrentState { get; private set; }
+
+        public bool IsDone { get; private set; }
+
+        public Engine()
+        {
+            IsDone = true;
+        }
+
+        public bool Init(string[] argv = null, string config = "Config.cfg")
+        {
+            IsDone = false;
+            return true;
+        }
+
+        public void SwitchState(State state)
+        {
+            if (CurrentState != null) CurrentState.End();
+
+            CurrentState = state;
+
+            if (CurrentState != null) CurrentState.Start();
+        }
+
+        public void CleanUp()
+        {
+            IsDone = true;
+        }
+    }
+}
---
+++ Src/Game.NET/States/Menu/MenuEntry.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Graphics;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Game.NET.States
+{
+    public class MenuEntry
+    {
+        public string Title;
+        public Menu.EntryAction Action;
+
+        public virtual void ProcessInput()
+        {
+            if (Input.GetButton(Input.Button.Use).IsDown && Action != null)
+                Action(this);
+        }
+
+        public virtual void Draw(Renderer renderer, Vector2 pos, float size, Color4 color)
+        {
+
+        }
+    }
+}
