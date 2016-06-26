a2e5e55b5885c957dfbb18f052e1b0a13f1d974e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-26 12:37:03 +0200

    Marking build as failed if something happend on packeging

--- Scripts/Pack.ps1
+++ Scripts/Pack.ps1
@@ -20,6 +20,6 @@ foreach ($project in $projects){
 }
 
 # Set build as failed if any error occurred
-#if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 
 write-host "Packeging finished"
