182f8901b0642327fc086fcc101e6ff80eccde11
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-26 12:35:35 +0200

    Added new script for nuget packaging

--- Build/BuildNugetPackage.cmd
+++
@@ -1,2 +0,0 @@
-nuget pack ..\Src\Game.NET\Game.NET.csproj -OutputDirectory .\
-nuget pack ..\Src\Game.NET.Parser\Game.NET.Parser.csproj -OutputDirectory .\
---
+++ Scripts/Pack.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version = 0
+)
+
+# projects to pack
+$projects = @(
+    "Src\Game.NET\Game.NET.csproj"
+)
+
+# pack function for project
+function Pack($path) {
+    nuget pack $path -Properties Configuration=Release -Version $version
+}
+
+write-host "Packeging started"
+# Pack each project
+foreach ($project in $projects){
+    write-host "Packing " $project "with suffix version " $version
+    Pack($project)
+}
+
+# Set build as failed if any error occurred
+#if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
+write-host "Packeging finished"
