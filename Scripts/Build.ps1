872b55ccc5760eb53ac40d27fbd2c212f49aadaf
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-07-03 18:37:05 +0200

    Invalid write-host information

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -14,9 +14,9 @@ function Build($path) {
     &$msbuildExe $path
 }
 
-write-host "Packeging started"
+write-host "Build started"
 foreach ($project in $projects){
-    write-host "Building " $project
+    write-host "Building "$project
     Build($project)
 }
 
